140ee4e6472f2aff6ae6b0afa905d766f0a56087
Y Nasr <yoyo.monem22@gmail.com>
2024-03-02 21:36:56 +0100

    Improve main.py much more (IT WORKS AGAIN!)

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import scratchattach as scratch3
-import os
+from termcolor import colored
 
-USERNAME = os.environ['username']
-PASSWORD = os.environ['password']
-
+sessionID = ".eJxVT8tOhDAU_ZeuZ7AtFMrsNEYTkzFqzERWpKUX6AAtwyNjNP67twmb2d2c1z3nl6wzTE4NQA6k8Os8Q_15IjtSqnVpy0CW1iDHeJoJlicZcgvMS-V9Z4Pp6qcOzK1Dq6oDF2wBA7fYSi3Wu2gj5ugDxn4DHzYx5no80ERpnplEGg2JTkyKXynVMadMatAyNoeLT9--7WNb0K-x2dvz6dI9vz8d8_rliDG9b6zb2xGTOMsizljEchbJOJTslWtW1YTm-GtHzBkBXy52gB_vAnw_wITV7l7hWhY47nZaq-YWRTLhqahjFlcZU3WSQq54bCpBqdB1xZUUAiQYQf7-AR_Xce4:1rgN3s:GlqAueVchPxBWIEnnZCw_h9laxg"
 projectID = "950783494"
 
-session = scratch3.login(USERNAME, PASSWORD)
+session = scratch3.Session(session_id=sessionID, username="YoussefTV")
 conn = session.connect_cloud(projectID)
 
 client = scratch3.CloudRequests(conn)
@@ -20,7 +18,8 @@ def balance(argument1):
             "The ScratchCredit API hasn't been implemented yet, but the request has still been ",
             f"recieved successfully either way. (Balance requested by {argument1})",
             "Your balance will be automatically set to 100 ScratchCredits when the API gets ",
-            "up and running."]
+            "up and running. If the API were to be implemented, it would've only outputted this:",
+            "100"]
 
 
 @client.request
@@ -35,7 +34,7 @@ def give_sc(argument1, argument2):
 
 @client.event
 def on_ready():
-    print("Request handler is running")
+    print("\033[1mScratchCredit's server has gone up and is currently receiving requests!\n\033[0mNow people can start using ScratchCredit!")
 
 
 @client.event
