d590a11b87e21c9f2dadd8693ff0c6dc45d09fbb
Y Nasr <youssef.land@outlook.com>
2025-01-03 17:29:25 +0200

    Update API.py

--- API.py
+++ API.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from local_simple_database import LocalDictDatabase
+from os import getenv
 import ast
 
 
@@ -83,6 +84,5 @@ def getHistory(user: str):
 
 
 if __name__ == '__main__':
-    import os
-    debug_mode = os.getenv('FLASK_ENV') == 'development'
+    debug_mode = getenv('FLASK_ENV') == 'development'
     app.run(host="0.0.0.0", port=81, debug=debug_mode)
